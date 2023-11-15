8ec0c69294666ba02358b75888355291001d0626
amaider <amaider@web.de>
2023-11-16 00:32:01 +0100

    big wave

---
+++ Finances/Preview Content/PreviewContainer.swift
@@ -0,0 +1,4 @@
+// 14.11.23, Swift 5.0, macOS 14.0, Xcode 15.0.1
+// Copyright © 2023 amaider. All rights reserved.
+
+import Foundation
---
+++ Finances/Views/ReceiptView.swift
@@ -0,0 +1,14 @@
+// 14.11.23, Swift 5.0, macOS 14.0, Xcode 15.0.1
+// Copyright © 2023 amaider. All rights reserved.
+
+import SwiftUI
+
+struct ReceiptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReceiptView()
+}
